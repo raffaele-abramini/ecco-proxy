a6c2760779c14912a58de5fe686344b6e750d3bd
oldirony <raffaele.abramini@gmail.com>
2021-02-20 14:54:59 +0000

    🏗 Add initial core logic

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,67 @@
-type ProxiedMethod<T extends object> = (originalArgs: any[], originalMethod: Function, originalObject: T) => any;
-type ProxiedProperty<T extends object> = (originalArgs: any[], originalObject: T) => any;
+type ProxiedMethod<T extends object> = (
+  originalArgs: any[],
+  originalMethod: Function,
+  originalObject: T
+) => any;
 
-export const EccoProxy = <T extends object>(objectToProxy: T, customHandlers: Partial<Record<keyof T, ProxiedMethod<T>|ProxiedProperty<T>>>) =>{
-    return new Proxy(objectToProxy, {
-        get: function (target, prop, receiver) {
-            if (typeof target[prop] ==="function") {
-                if (customHandlers[prop]){
-                return (...args) => {
-                    return customHandlers[prop](args, Reflect.get(target, prop, receiver), receiver)
-                } }
-            } else {
+type ProxiedProperty<T extends object> = (
+  originalProperty: any,
+  originalObject: T
+) => any;
 
-            }
-        },
-    })
-}
+export const EccoProxy = <T extends object>(
+  objectToProxy: T,
+  customHandlers: Partial<
+    Record<keyof T, ProxiedMethod<T> | ProxiedProperty<T>>
+  >
+) => {
+  return new Proxy(objectToProxy, {
+    get: function (target, prop, receiver) {
+      const customHandler = customHandlers[prop];
+
+      if (typeof target[prop] === "function") {
+        if (customHandler) {
+          return (...args) => {
+            return customHandler(
+              args,
+              Reflect.get(target, prop, receiver),
+              receiver
+            );
+          };
+        }
+        return Reflect.get(target, prop, receiver);
+      } else {
+        if (customHandler) {
+          return customHandler(target[prop], receiver);
+        }
+        return target[prop];
+      }
+    },
+  });
+};
 
 const TestObj = {
-    isActive: false,
-    addListener: (...args: any) => {
-        console.log("bubu", ...args);
-    }
-}
+  isActive: false,
+  isOnline: true,
+  logSomething: (...args: any) => {
+    console.log("random", ...args);
+  },
+  addListener: (...args: any) => {
+    console.log("bubu", ...args);
+  },
+};
 
 const p = EccoProxy(TestObj, {
-    addListener: (originalArgs, originalMethod) => {
-        originalMethod(...originalArgs);
-        console.log("cane", ...originalArgs.map(Math.random));
-    },
-})
+  addListener: (originalArgs, originalMethod) => {
+    console.log("cane", ...originalArgs.map(Math.random));
+    return originalMethod(...originalArgs);
+  },
+  isActive: (originalProperty) => {
+    return String(originalProperty);
+  },
+});
 
-p.addListener(1,2,3);
+p.addListener(1, 2, 3);
+console.log(p.isActive);
+console.log(p.isOnline);
+p.logSomething(2, 23, 4);
